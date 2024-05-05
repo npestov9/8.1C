a774129ecdc9e76c698f185e3c3886afd39784e6
Nikita Pestov <nikitapestov@nikitas-mbp.lan>
2024-05-05 21:10:34 +1000

    final commit

--- app/src/main/java/com/example/a81c/ChatActivity.java
+++ app/src/main/java/com/example/a81c/ChatActivity.java
@@ -9,7 +9,18 @@ import android.widget.EditText;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -37,29 +48,78 @@ public class ChatActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String message = messageEditText.getText().toString();
                 if (!message.isEmpty()) {
-                    messages.add("You: " + message);  // Add user message to the list
-                    messageAdapter.notifyDataSetChanged();  // Notify the adapter to refresh the list view
-                    messageEditText.setText("");  // Clear the input box after sending
-                    sendMessageToServer(message);  // Method to send the message to the server
+                    messages.add("You: " + message);
+                    messageAdapter.notifyDataSetChanged();
+                    messageEditText.setText("");
+                    sendMessageToServer(message);
                 }
             }
         });
     }
 
     private void sendMessageToServer(String userMessage) {
-        // Here, you would add your network call logic to send the message to the server
-        // For now, let's simulate a server response
-        String simulatedResponse = "This is a simulated response from the server.";
-        displayServerResponse(simulatedResponse);
+        final String xml = "<chat application='8332384265189409422' instance='165'><message>" + userMessage + "</message></chat>";
+        final MediaType MEDIA_TYPE_XML = MediaType.parse("application/xml; charset=utf-8");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                RequestBody body = RequestBody.create(xml, MEDIA_TYPE_XML);
+                Request request = new Request.Builder()
+                        .url("https://www.botlibre.com/rest/api/chat")
+                        .post(body)
+                        .build();
+
+                try {
+                    Response response = client.newCall(request).execute();
+                    if (response.isSuccessful() && response.body() != null) {
+                        String responseData = response.body().string();
+                        String extractedMessage = parseXML(responseData);
+                        displayServerResponse(extractedMessage);
+                    } else {
+                        runOnUiThread(() -> {
+                            messages.add("Bot: Error - " + response.code());
+                            messageAdapter.notifyDataSetChanged();
+                        });
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    runOnUiThread(() -> {
+                        messages.add("Bot: Failed to connect: " + e.getMessage());
+                        messageAdapter.notifyDataSetChanged();
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private String parseXML(String xmlData) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+
+            parser.setInput(new StringReader(xmlData));
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG && "message".equals(parser.getName())) {
+                    if (parser.next() == XmlPullParser.TEXT) {
+                        return parser.getText();  // Return the text inside <message>
+                    }
+                }
+                eventType = parser.next();
+            }
+        } catch (Exception e) {
+            Log.e("ChatActivity", "Error parsing XML", e);
+            return "Error parsing response";
+        }
+        return "No message found";
     }
 
     private void displayServerResponse(String message) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                messages.add("Bot: " + message);  // Add server response to the list
-                messageAdapter.notifyDataSetChanged();  // Notify the adapter to refresh the list view
-            }
+        runOnUiThread(() -> {
+            messages.add("Bot: " + message);
+            messageAdapter.notifyDataSetChanged();
         });
     }
 }
